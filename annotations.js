da42ac607a0fccccda8f07a50bbb92bc70ae8491
Anay Nayak <anayak007@gmail.com>
2018-07-26 08:54:44 +0530

    Adding tests

--- annotations.js
+++ annotations.js
@@ -1,18 +1,27 @@
 
 function addAnnotations(json, config) {
-    json.widgets.forEach(w => addAnnotation(w.properties, config));
+    var cfg = Object.assign({}, {
+        limit: 3
+    }, config)
+    json.widgets.forEach(w => addAnnotation(w.properties, cfg));
     return json;
 }
 
 function addAnnotation(properties, config) {
     properties.annotations = properties.annotations || {};
-    properties.annotations.vertical = [
+    properties.annotations.vertical = properties.annotations.vertical || [];
+    processAnnotations(properties.annotations, config)
+}
+
+function processAnnotations(annotations, config) {
+    annotations.vertical.push(
         {
-            label: "Deployment",
+            label: config.label || "Deployment",
             source: 'aws-cloudwatch-annotations',
-            value: "2018-06-26T02:56:36.000Z"
+            value: config.value || new Date().toISOString()
         }
-    ];
+    );
+    annotations.vertical = annotations.vertical.slice(-config.limit);
 }
 
 module.exports = {
---
+++ test/annotations.test.js
@@ -0,0 +1,106 @@
+var Annotations = require('../annotations')
+
+test('should do nothing if there are no widgets', () => {
+    expect(Annotations.addTo({
+        widgets: []
+    })).toEqual({ widgets: [] });
+});
+
+test('should add specified annotation to the widget', () => {
+    expect(Annotations.addTo({
+        widgets: [{
+            title: "widget1",
+            properties: {}
+        }]
+    }, { value: "2018-06-26T02:56:36.000Z" })).toEqual({
+        widgets: [{
+            title: "widget1",
+            properties: {
+                annotations: {
+                    vertical: [
+                        {
+                            label: "Deployment",
+                            source: "aws-cloudwatch-annotations",
+                            value: "2018-06-26T02:56:36.000Z",
+                        }
+                    ]
+                }
+            }
+        }]
+    });
+});
+
+test('should limit added annotations to the widget', () => {
+    expect(Annotations.addTo({
+        widgets: [{
+            title: "widget1",
+            properties: {
+                annotations: {
+                    vertical: [
+                        {
+                            label: "Older Deployment",
+                            source: "aws-cloudwatch-annotations",
+                            value: "2018-06-26T02:56:36.000Z",
+                        },
+                    ]
+                }
+            }
+        }]
+    }, { limit: 1, value: "2018-08-26T12:56:36.100Z" })).toEqual({
+        widgets: [{
+            title: "widget1",
+            properties: {
+                annotations: {
+                    vertical: [
+                        {
+                            label: "Deployment",
+                            source: "aws-cloudwatch-annotations",
+                            value: "2018-08-26T12:56:36.100Z",
+                        },
+                    ]
+                }
+            }
+        }]
+    });
+});
+
+test('should not delete existing annotations if within limit', () => {
+    expect(Annotations.addTo({
+        widgets: [{
+            title: "widget1",
+            properties: {
+                annotations: {
+                    vertical: [
+                        {
+                            label: "Older Deployment",
+                            source: "aws-cloudwatch-annotations",
+                            value: "2018-06-26T02:56:36.000Z",
+                        },
+                    ]
+                }
+            }
+        }]
+    }, { limit: 2, value: "2018-08-26T12:56:36.100Z" })).toEqual({
+        widgets: [{
+            title: "widget1",
+            properties: {
+                annotations: {
+                    vertical: [
+                        {
+                            label: "Older Deployment",
+                            source: "aws-cloudwatch-annotations",
+                            value: "2018-06-26T02:56:36.000Z",
+                        },
+                        {
+                            label: "Deployment",
+                            source: "aws-cloudwatch-annotations",
+                            value: "2018-08-26T12:56:36.100Z",
+                        }
+                    ]
+                }
+            }
+        }]
+    });
+});
+
+// test('should not get rid of other annotations')
