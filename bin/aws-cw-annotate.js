5eeba5752a44eee5968b7b3265bcbf9fecdc46bd
Anay Nayak <anayak007@gmail.com>
2018-07-26 21:10:38 +0530

    Help text

--- bin/aws-cw-annotate.js
+++ bin/aws-cw-annotate.js
@@ -1,9 +1,25 @@
 #! /usr/bin/env node
 
-var config = require('minimist')(process.argv.slice(2));
+var config = require('minimist')(process.argv.slice(2), {
+    string: ['fill', 'color', 'widget-title'],
+    default: { 'widget-title': '.*', limit: 5 }
+});
 var Reader = require('../lib/reader');
 var Annotations = require('../lib/annotations');
 
-Reader.read(function(json) {
-    console.log(JSON.stringify(Annotations.addTo(json, config)));
-})
+if (config._ == 'help') {
+    console.log(`
+Usage:
+    aws cloudwatch get-dashboard --dashboard-name $SOURCE_DASHBOARD --query 'DashboardBody' --output text | aws-cw-annotate
+Options:
+    --fill              Fill value for vertical annotation (before/after/between)
+    --color             Annotation color
+    --widget-title      Update only widgets whose title matching specified regex 
+    --limit             Max number of aws-cw-annotate annotations to maintain
+`);
+}
+if (!config._.length) {
+    Reader.read(function (json) {
+        console.log(JSON.stringify(Annotations.addTo(json, config)));
+    });
+}
